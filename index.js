cf4e94e6dd4b32fe73e11a1ba56272cc89d52386
Ex Cinera <ashes@phoeni.city>
2018-10-17 01:39:34 -0400

    Properly orders the list of RSS entries to queue for tweeting. Creates and maintains file of GUIDs from RSS entries to avoid duplicating previous posts. I cannot seem to make it stop batching multiple tweets. It insists on posting them simultaneously in a random order from what they're sent in, regardless of how long a delay the individual post commands are spaced out by.

--- index.js
+++ index.js
@@ -3,15 +3,16 @@
 // sorry
 // gpl v2.0
 
-var fs = require('fs');	
+var fs = require('fs'); 
 var Twitter = require('twitter');
 var Parser = require('rss-parser');
+var now = require('moment');
 
 // Check for verbose and debug flags, set variables
 var v = (process.argv.indexOf('v') + process.argv.indexOf('-v') + process.argv.indexOf('--verbose') + process.argv.indexOf('verbose') + 4);
 var d = (process.argv.indexOf('d') + process.argv.indexOf('-d') + process.argv.indexOf('--debug') + process.argv.indexOf('debug') + 4);
 v = v ? 1 : 0;
-d = d ? 1 : 0;
+d = d ? 1 : 0;  
 (d || v) && console.log("Starting with " + (d ? "debug " : "") + ((d * v) ? "and " : "") + (v ? "verbose " : "") + "mode enabled");
 
 var parser = new Parser();
@@ -22,33 +23,45 @@ var rssUrl = config.rss_url;
 var twitName = config.twit_name;
 const clientAuth = JSON.parse(fs.readFileSync(__dirname + "/clientAuth.json"));
 var client = new Twitter(clientAuth);
+var guids = JSON.parse(fs.readFileSync(__dirname + "/guids.log"));
 v && console.log("Connecting");
 client.get('statuses/user_timeline', '{screen name: ' + twitName + '}', function(error, tweets, response) {
   if (!error) {
-  	console.log(tweets);
-  	v && console.log("No error");
+   //console.log(tweets);
+   v && console.log("No error");
    (async () => {
     let feed = await parser.parseURL(rssUrl);
-    feed.items.forEach(item => {
-     v && console.log('Here is an item');
-     client.post('statuses/update', {status: '"' + item.title + '", by ' + item.creator + ": "+ item.link})
+    var smeti = feed.items.reverse();
+    console.log(guids);
+    var i = 0;
+    smeti.forEach(item => {
+     v && console.log(item.isoDate + ", " + item.guid + ": " + item.title + "(" + guids.indexOf(item.guid) + ")");
+     if(guids.indexOf(item.guid) == -1) {
+      for(var j = -1; j < 1000*1000*2000*i; j++) {}
+      guids.push(item.guid);
+      v && console.log(item.guid);
+      v && console.log(now().format('MM/DD/YYYY @ HH:mm:ss') + " | Posting.");
+      client.post('statuses/update', {status: '"' + item.title + '", by ' + item.creator + ": "+ item.link})
       .then(function (tweet) {
-       console.log(tweet.text);
+       console.log(now().format('MM/DD/YYYY @ HH:mm:ss') + " | " + tweet.text);
       }) // closes tweet
       .catch(function (error) {
        throw error;
       }) // closes error catch */
+     i++;
+     } // closes "if untweeted GUID"
+     fs.writeFileSync(__dirname + "/guids.log", JSON.stringify(guids));
     }); // closes forEach in feed
    })(); // closes async
    for (var n in tweets){
-    console.log(tweets[n]['user']['screen_name'] + ": " + tweets[n]['text']);
+    //console.log('a');
+    //console.log(tweets[n]['user']['screen_name'] + ": " + tweets[n]['text']);
    }
   } // closes if (!error)
 }); // closes client.get
 
 
 console.log(rssUrl);
-
 
 /*
 client.post('statuses/update', {status: 'I Love Twitter'},  function(error, tweet, response) {
