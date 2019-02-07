9e12d9455eb9030c1b280abedbc5475d92eaab3c
Ex Cinera <ashes@phoeni.city>
2019-02-06 21:53:10 -0500

    Fix bug in multi-feed parsing and single-tweet flags.

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-// diptwit 1.2
+const versionid = "1.2.1";
+
 // x. 2k18 10 01 - 2k18 10 17
 // 2k18 12 30: fixed the post modes
+// 2k19 02 03: multi-url fetching
+// 2k19 02 06: fix multi-url fetching
 // gpl v3.0
 
-var fs = require('fs'); 
+var fs = require('fs');
+var path = require('path');
 var Twitter = require('twitter');
 var Parser = require('rss-parser');
 var now = require('moment');
-const versionid = "1.2";
 
 fs.appendFileSync(__dirname + "/run.log", "\n" + now().format("[YYYY-MM-DD") + "T" + now().format("HH:mm:ss]") + " STARTED");
 
@@ -17,6 +20,7 @@ var d = (process.argv.indexOf('d') + process.argv.indexOf('-d') + 2);
 var r = (process.argv.indexOf('r') + process.argv.indexOf('-r') + 2);
 var s = (process.argv.indexOf('s') + process.argv.indexOf('-s') + 2);
 var h = (process.argv.indexOf('h') + process.argv.indexOf('-h') + 2);
+
 v = v ? 1 : 0;
 d = d ? 1 : 0;  
 r = r ? 1 : 0; 
@@ -24,7 +28,7 @@ s = s ? 1 : 0;
 h = h ? 1 : 0;  
 
 if (h) {
- console.log(versionid + " / x. cinera 2k19 / GPL v3");
+ console.log("diptwit " + versionid + " / x. cinera 2k19 / GPL v3");
  console.log("Usage: node " + path.basename(__filename) + " [-d] [-h] [-s] [-v]")
  console.log("  -d, debug      Run in debug mode")
  console.log("  -h, help       Display help menu and exit")
@@ -33,8 +37,6 @@ if (h) {
  console.log("  -v, verbose    Run with verbose logging enabled")
  process.exit(0);
  } // If help flag is set, print command flags and exit.
-
-
 
 var parser = new Parser();
 
@@ -90,6 +92,8 @@ var rssUrls = config.rss_urls;
 var twitName = config.twit_name;
 var timeout = config.timeout;
 var client = new Twitter(clientAuth);
+var feed = [];
+var smeti = [];
 
 // No, Mr. Node.js, I expect you to die.
 setTimeout(function() {
@@ -102,41 +106,42 @@ s && console.log("Running in single-post mode over " + rssUrls.length + "RSS URL
 !s && console.log("Running in multi-post mode over " + rssUrls.length + "RSS URLs.");
 (d || v) && console.log("Starting with " + (d ? "debug " : "") + ((d * v) ? "and " : "") + (v ? "verbose " : "") + "mode enabled");
 var skipAllRest = 0;
-var skipTheRest = 0;
-for (var asdf in rssUrls) {
- skipTheRest = 0;
- s && fs.appendFileSync(__dirname + "/run.log", " [in 1-post mode] from: " + rssUrls[asdf] + " with timeout " + timeout + "\n");
- !s && fs.appendFileSync(__dirname + "/run.log", " [in n-post mode] from: " + rssUrls[asdf] + " with timeout " + timeout + "\n");
- 
- // Actually getting the tweet.
+var skipTheRest = []; 
+ // Actually getting the tweets.
  client.get('statuses/user_timeline', '{screen name: ' + twitName + '}', function(error, tweets, response) {
-   if (!error) {
-    //console.log(tweets);
-    d && console.log("No error");
-    (async () => {
-     let feed = await parser.parseURL(rssUrls[asdf]);
-     var smeti = feed.items.reverse();
-     d && console.log(guids);
+  if (!error) {
+   d && console.log("No error");
+   for (var asdf in rssUrls) {
+    console.log(asdf + " RQ RSS " + rssUrls[asdf]);
+    s && fs.appendFileSync(__dirname + "/run.log", " [in 1-post mode] from: " + rssUrls[asdf] + " with timeout " + timeout + "\n");
+    !s && fs.appendFileSync(__dirname + "/run.log", " [in n-post mode] from: " + rssUrls[asdf] + " with timeout " + timeout + "\n");
+    parser.parseURL(rssUrls[asdf])
+    .then(function (items) {
+     skipTheRest[items.feedUrl] = 0;
+     console.log(items.feedUrl.length);
+     console.log(items.feedUrl.length + " RX RSS " + rssUrls[asdf]);
+     smeti[asdf] = items['items'].reverse();
+     // d && console.log(guids);
      // var i = 0;
-     for(var k in smeti){
-      item = smeti[k];
-      d && console.log(item.isoDate + ", " + item.guid + ": " + item.title + " (" + guids.indexOf(item.guid) + ")");
+     for(var k in smeti[asdf]){
+      item = smeti[asdf][k];
+      d && console.log(items.feedUrl.length + " ITEM : " + item.isoDate + ", " + item.guid + ": " + item.title + " (" + guids.indexOf(item.guid) + ")");
       if(guids.indexOf(item.guid) == -1) {
        // for(var j = -1; j < 1000*1000*2000*i; j++) {}
-       k = smeti.length;
-       (d || v) && console.log(now().format("[YYYY-MM-DD") + "T" + now().format("HH:mm:ss]") + " GUID " + item.guid + " skip: " + skipAllRest);
+       k = smeti[asdf].length;
        var count = 0;
        count += item.content.split(" ").length - 1;
-       d && console.log(count + " words");
+       (d || v) && console.log(items.feedUrl.length + " NEW  : " + now().format("YYYY-MM-DD") + "T" + now().format("HH:mm:ss") + " GUID " + item.guid + " skip: " + skipAllRest + "/" + skipTheRest[items.feedUrl] + " ( " + count + " words)");
        count = Math.floor(count / 300.0);
        if (count === 0) count++;
-       if (skipAllRest === 0 && skipTheRest === 0) {
+       if (skipAllRest === 0 && skipTheRest[items.feedUrl] === 0) {
         s && skipAllRest++;
-        r && skipTheRest++;
+        r && skipTheRest[items.feedUrl]++;
         guids.push(item.guid);
+        console.log("tweetan from " + items.feedUrl);
         client.post('statuses/update', {status: '"' + item.title + '", by ' + item.creator + " (" + count + "m read) "+ item.link})
         .then(function (tweet) {
-         (d || v) && console.log(now().format("[YYYY-MM-DD") + "T" + now().format("HH:mm:ss]") + " " + tweet.text);
+         (d || v) && console.log(items.feedUrl.length + " TWEET: " + now().format("[YYYY-MM-DD") + "T" + now().format("HH:mm:ss]") + " " + tweet.text);
          fs.appendFileSync(__dirname + "/run.log", now().format("[YYYY-MM-DD") + "T" + now().format("HH:mm:ss]") + " SENT " + tweet.text + "\n");
          fs.writeFileSync(__dirname + "/guids.log", JSON.stringify(guids));
          }) // closes tweet
@@ -148,21 +153,10 @@ for (var asdf in rssUrls) {
         else if (skipAllRest > 0) {
         (d || v) && console.log("Terminating program.");
         process.exit(0);
-        }
+        } // if skipAllRest is set
       } // closes "if untweeted GUID"
      } // closes increment loop over smeti
-    })(); // closes async
-    for (var n in tweets){
-     //console.log('a');
-     //console.log(tweets[n]['user']['screen_name'] + ": " + tweets[n]['text']);
-    }
-   } // closes if (!error)
+    }); // closes then-block for handling rss feed items
+   } // closes iterator over all urls
+  } // closes if (!error)
  }); // closes client.get
- console.log("RSS url: " + rssUrls[asdf]);
- /*
- client.post('statuses/update', {status: 'test'},  function(error, tweet, response) {
-   if(error) throw error;
-   console.log(tweet);  // Tweet body.
-   console.log(response);  // Raw response object.
- }); // closes client.post */
-}
